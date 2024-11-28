2631d5d88fcb129182ca964ba93bad39aa231df2
Vasile <florin777312@gmail.com>
2024-11-28 08:33:59 +0200

    Made change color button functional
    Changes color to black or gives random colors

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 
 const container = document.querySelector('.container');
 const initialDimension = 16;
+let currentColor = 1;
 
+let randomColor = () => {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r} ${g} ${b})`;
+}
 
 for (let i = 0; i < initialDimension * initialDimension; i++){
     const d = document.createElement('div');
@@ -13,7 +20,10 @@ for (let i = 0; i < initialDimension * initialDimension; i++){
 function addEvents(nodes){
     nodes.forEach(function(el) {
         el.addEventListener('mouseover', () => {
+            if (currentColor === 1)
             el.style.backgroundColor = 'black';
+            else
+            el.style.backgroundColor = randomColor();
         });
     });
 }
@@ -60,3 +70,17 @@ changeSize.addEventListener('click',()=>{
     addEvents(nodes);
 });
 
+const changeColor = document.querySelector(".change-color");
+changeColor.textContent = 'Change color to random';
+
+
+changeColor.addEventListener('click', () => {
+    if (currentColor === 1){
+        currentColor = 2;
+        changeColor.textContent = 'Change color to black';
+    }
+    else{
+        currentColor = 1;
+        changeColor.textContent = 'Change color to random';
+    }
+});
