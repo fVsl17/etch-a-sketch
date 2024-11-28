66ed04f197a1cfab4da758c4117bd7e82d2f0406
Vasile <florin777312@gmail.com>
2024-11-28 07:04:39 +0200

    Reworked everything from scratch
    Made basic layout and added hover and reset button

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
-'use strict';
+'use strict'
 
-const container = document.querySelector(".container");
-const initialGridDimensions = 64;
-let finalGridDimensions = initialGridDimensions * initialGridDimensions;
-container.setAttribute('style', `display:flex; flex:none; flex-basis:457px; height:200px; flex-wrap:wrap; align-items:center; justify-content:center;`);
+const container = document.querySelector('.container');
+
+const initialDimension = 16;
 
 
-for (let i = 0; i < finalGridDimensions; i++){
-    const grid = document.createElement('div');
-    grid.classList.add('grid');
-    container.appendChild(grid);
+for (let i = 0; i < initialDimension * initialDimension; i++){
+    const d = document.createElement('div');
+    d.classList.add('grid-element');
+    container.appendChild(d);
 }
 
-const grid = document.querySelectorAll('.grid');
-
-grid.forEach(function(el) {
-    el.setAttribute('style', `border:1px solid black; height:16px; width:16px;`);
+container.childNodes.forEach(function(el) {
+    el.addEventListener('mouseover', () => {
+        el.style.backgroundColor = 'black';
+    });
 });
 
+const resetButton = document.querySelector('.reset');
 
-grid.forEach(function(el) {
-    el.addEventListener('mouseover', (e) => {
-        let c1 = Math.floor(Math.random() * 255);
-        let c2 = Math.floor(Math.random() * 255);
-        let c3 = Math.floor(Math.random() * 255);
-        e.target.style.backgroundColor = `rgb(${c1} ${c2} ${c3})`;
+resetButton.addEventListener('click', ()=>{
+    container.childNodes.forEach((el) => {
+        el.style.backgroundColor = 'white';
     });
 });
